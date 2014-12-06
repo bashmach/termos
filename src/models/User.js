4f625e9922acbfb14c627c0c063eee17c741ee2b
Pavel Machekhin <bashmach@gmail.com>
2014-12-06 15:56:53 +0000

    Cleanup

--- src/models/User.js
+++ src/models/User.js
@@ -6,24 +6,15 @@ var userSchema = new mongoose.Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String,
 
-  facebook: String,
   twitter: String,
-  google: String,
-  github: String,
-  instagram: String,
-  linkedin: String,
   tokens: Array,
 
   profile: {
     name: { type: String, default: '' },
     gender: { type: String, default: '' },
     location: { type: String, default: '' },
-    website: { type: String, default: '' },
-    picture: { type: String, default: '' }
-  },
-
-  resetPasswordToken: String,
-  resetPasswordExpires: Date
+    website: { type: String, default: '' }
+  }
 });
 
 /**
